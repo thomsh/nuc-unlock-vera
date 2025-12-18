3606ad4e50d0751786b72d3cb6fc9739e187203e
thom-vend <thomsh@vendhq.com>
2025-12-18 17:53:53 +1300

    Cleanup integration test

--- integration_tests.sh
+++ integration_tests.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 # Simple integration test
 set -eEuo pipefail
+exit_code=1 # bad by default
 srvpid=""
 output=$(mktemp)
 function exit_trap {
     rm -f -- "$output"
     kill "$srvpid"
     echo cleaned
+    exit $exit_code
 }
-trap exit_trap EXIT
+trap exit_trap EXIT ERR
 pushd tests
 ./start-python-server.sh &
 srvpid=$!
 popd
 sleep 0.1
+echo "📝 test1: run unlock mode"
 go run main.go -m unlock -r 2>&1 |tee "$output"
 # Check output
 if ! grep -Fq 'word 1 word 2 word 3' "$output"; then
@@ -26,17 +29,23 @@ if ! grep -Fq 'NUC unlocked' "$output"; then
 fi
 echo "Test for mode unlock passed ✅"
 
-# test for encrypt/decrypt mode
+echo "📝 test2: encrypt mode"
 go run main.go -m encrypt -p "UT0EVP😆xStu3q" -d "0BGG7zyFqhauu42tESRMtlBt92C1tYaF"  2>&1 |tee "$output"
 ciphertext=$(grep -A 1 -F -- '----COPY FROM HERE----' "$output" |tail -n 1)
-# decrypt
+
+echo "📝 test3: decrypt mode"
 go run main.go -m decrypt -p "UT0EVP😆xStu3q" -d "$ciphertext"  2>&1 |tee "$output"
 if ! grep -Fq '0BGG7zyFqhauu42tESRMtlBt92C1tYaF' "$output"; then
     echo "failed ❌ expected decrypted text not found"
     exit 1
 fi
+
+echo "📝 test4: decrypt mode with empty password"
 if go run main.go -m decrypt -p "" -d "$ciphertext"; then
     echo "failed ❌ expected error when password is empty"
     exit 1
 fi
 echo "Test for mode encrypt & decrypt passed ✅"
+
+echo "🏁 all done"
+exit_code=0
